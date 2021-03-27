a49332ebead5b8bc3f6ebc200ff01a376456e0aa
Arman Ozak <arman@armanozak.com>
2021-03-27 17:48:42 +0300

    add structure and styles to main layout

--- projects/demo/src/app/layouts/main-layout/main-layout.module.ts
+++ projects/demo/src/app/layouts/main-layout/main-layout.module.ts
@@ -1,11 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { MainLayoutComponent } from './main-layout.component';
 
 @NgModule({
   exports: [MainLayoutComponent],
   declarations: [MainLayoutComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
 })
 export class MainLayoutModule {}
