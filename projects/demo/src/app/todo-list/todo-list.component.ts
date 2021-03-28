f052a794b85d24e052c8ded20910ef6603307d2d
Arman Ozak <arman@armanozak.com>
2021-03-28 13:06:05 +0300

    implement record deletion

--- projects/demo/src/app/todo-list/todo-list.component.ts
+++ projects/demo/src/app/todo-list/todo-list.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import type { Rec, Todo } from '@ng101/mock';
-import { merge, of, Subject } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { EMPTY, merge, of, Subject } from 'rxjs';
+import { concatMap, switchMap } from 'rxjs/operators';
 import { TodoService } from './todo.service';
 
 @Component({
@@ -10,17 +11,32 @@ import { TodoService } from './todo.service';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent {
+  @ViewChild('deleteDialog')
+  private deleteDialog?: TemplateRef<any>;
+
   private listUpdate$ = new Subject<void>();
 
   list$ = merge(of(0), this.listUpdate$).pipe(
     switchMap(() => this.todo.getList())
   );
 
-  constructor(private todo: TodoService) {}
+  constructor(private todo: TodoService, private dialog: MatDialog) {}
 
   toggleDone(todo: Rec<Todo>) {
     this.todo
       .update(todo.id, { title: todo.title, done: !todo.done })
+      .subscribe(() => this.listUpdate$.next());
+  }
+
+  deleteRecord(todo: Rec<Todo>) {
+    this.dialog
+      .open(this.deleteDialog!, { data: todo.title })
+      .afterClosed()
+      .pipe(
+        concatMap((confirmed) =>
+          confirmed ? this.todo.delete(todo.id) : EMPTY
+        )
+      )
       .subscribe(() => this.listUpdate$.next());
   }
 }
--- projects/demo/src/app/todo-list/todo-list.module.ts
+++ projects/demo/src/app/todo-list/todo-list.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -17,6 +18,7 @@ import { TodoListComponent } from './todo-list.component';
     MainLayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatIconModule,
     MatListModule,
     MatProgressSpinnerModule,
